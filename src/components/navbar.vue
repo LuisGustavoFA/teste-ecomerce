015521e5452ff12f55b3eb76da14262acad7566f
João Gabriel FA <joaogabrielfa.dev@gmail.com>
2025-04-21 00:06:17 -0300

    Criando navbar

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-
+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaa
   </div>
 </template>
 
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,8 +1,10 @@
 <template>
   <h1>Home</h1>
+  <Navbar/>
 </template>
 
 <script>
+import { Navbar } from "../components/Navbar.vue";
 export default {
   name: 'HomePage',
 }
